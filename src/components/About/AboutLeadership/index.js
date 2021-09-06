d9cf513fa73e303392688d666acbd16df6249216
ernestpapyan <ernestpapyan96@gmail.com>
2021-09-06 15:49:33 +0400

    second bug list fixed

--- src/components/About/AboutLeadership/index.js
+++ src/components/About/AboutLeadership/index.js
@@ -7,7 +7,7 @@ const AboutLeadership = () => (
       <p>Leadership</p>
       <div className="about-leadership-items flex">
         <div className="about-leadership-items-photo flex-column">
-          <img src={HERBERT} alt="Herbert" style={{borderRadius: '50%'}}/>
+          <img src={HERBERT} alt="Herbert" style={{borderRadius: '50%'}} width={230}/>
           <div className="about-leadership-items-photo-name">
             Herbert
           </div>
@@ -16,7 +16,7 @@ const AboutLeadership = () => (
           </div>
         </div>
         <div className="about-leadership-items-photo flex-column">
-          <img src={KARIN} alt="Karin" style={{borderRadius: '50%'}} />
+          <img src={KARIN} alt="Karin" style={{borderRadius: '50%'}} width={230}/>
           <div className="about-leadership-items-photo-name">
             Karin Stephan
           </div>
--- src/components/About/AboutTeam/index.js
+++ src/components/About/AboutTeam/index.js
@@ -6,7 +6,7 @@ const AboutTeam = () => (
       <p>Earkick team</p>
       <div className="about-team-items flex">
         <div>
-          <a href={"https://www.linkedin.com/in/herbertbay/"}>Evan Nisselson</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Herbert Bay</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Karin Stephan</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Igor Makara</a> - <br/><a href={"https://www.linkedin.com/in/herbertbay/"}>Roy Ashok</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Srivignesh Rajendran</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Veronique Larcher</a>
+          <a href={"https://www.linkedin.com/in/herbertbay/"}>Evan Nisselson</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Herbert Bay</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Karin Stephan</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Igor Makara</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Roy Ashok</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Srivignesh Rajendran</a> - <a href={"https://www.linkedin.com/in/herbertbay/"}>Veronique Larcher</a>
         </div>
       </div>
     </div>
